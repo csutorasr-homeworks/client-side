7d6548c90ef0b95c8a3b89b2d9bee2b4d073641c
Csutorás Robin <csutorasr@gmail.com>
2018-04-27 11:44:23 +0200

    feat: error interceptor is added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { TokenInterceptor } from './token.interceptor';
+import { ErrorInterceptor } from './error.interceptor';
 import { UserService } from './user.service';
 import { DriveService } from './drive.service';
 import { AuthGuard } from './auth.guard';
@@ -15,6 +16,11 @@ import { UnauthGuard } from './unauth.guard';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
       multi: true
     },
     UserService,
